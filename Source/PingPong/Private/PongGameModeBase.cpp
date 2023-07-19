50131b1e9d767ab2915894848eb9965e5ff2a1b2
Saygachenko <curatoroff@gmail.com>
2023-07-19 21:37:03 +0300

    Added a PongGameModeBase.

---
+++ Source/PingPong/Private/PongGameModeBase.cpp
@@ -0,0 +1,62 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PongGameModeBase.h"
+#include "Kismet/GameplayStatics.h"
+
+void APongGameModeBase::StartGame() const
+{
+	TArray<AActor*> BallArray;
+	//UGameplayStatics::GetAllActorsOfClass(this, APongBall::StaticClass(), BallArray);
+	//if (BallArray.Num() > 0 && IsValid(BallArray[0]))
+	//{
+	//	Cast<APongoBall>(BallArray[0])->Start();
+	//}
+}
+
+void APongGameModeBase::EndGame()
+{
+	TArray<AActor*> BallArray;
+	//UGameplayStatics::GetAllActorsOfClass(this, APongBall::StaticClass(), BallArray);
+	//if (BallArray.Num() > 0 && IsValid(BallArray[0]))
+	//{
+	//	Cast<APongoBall>(BallArray[0])->EndGame();
+	//}
+}
+
+void APongGameModeBase::PostLogin(APlayerController* NewPlayer)
+{
+	Super::PostLogin(NewPlayer);
+
+	TArray<AActor*> PawnArray;
+	//UGameplayStatics::GetAllActorsOfClass(this, APongPlayer::StaticClass(), PawnArray);
+	if (PawnArray.Num() > 0)
+	{
+		for (AActor* Actor : PawnArray)
+		{
+			//APongPlayer* PongPlayer = Cast<APongPlayer>(Actor);
+			//if (PongPlayer && !PongPlayer->IsPawnControlled())
+			//{
+			//	NewPlayer->Possess(PongPlayer);
+			//	PongPlayer->SetOwner(NewPlayer);
+			//}
+		}
+	}
+	if (GetNumPlayers() == 2)
+	{
+		StartGame();
+	}
+}
+
+void APongGameModeBase::Logout(AController* Exiting)
+{
+	EndGame();
+
+	const auto World = GetWorld();
+	if (!World)
+	{
+		return;
+	}
+
+	World->ServerTravel("Game/Maps/Game?listen");
+}
---
+++ Source/PingPong/Public/PongGameModeBase.h
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PongGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PINGPONG_API APongGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	void StartGame() const;
+	void EndGame();
+
+protected:
+	virtual void PostLogin(APlayerController* NewPlayer) override;
+	virtual void Logout(AController* Exiting) override;
+
+	int ConnetctedPlayers{};
+};
